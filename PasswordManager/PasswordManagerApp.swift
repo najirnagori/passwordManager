18d6e07e99b71c2abdade868a2e310bd88538d7b
Najir <mohammed.najir@applligent.com>
2024-09-13 12:12:49 +0530

    Initial Commit

---
+++ PasswordManager/PasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerApp.swift
+//  PasswordManager
+//
+//  Created by Najir on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
